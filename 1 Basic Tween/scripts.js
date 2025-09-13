304aefbab36536130081328c1e3c48d4ba9cd820
Kaif <kaif49490@gmail.com>
2025-09-13 21:18:41 +0530

    Basic Tweens

---
+++ 1 Basic Tween/scripts.js
@@ -0,0 +1,3 @@
+// Documentation of GSAP (GreenSock Animation Platform)   https://gsap.com/docs/v3/GSAP/Tween/?ref=6234
+
+gsap.to(".circle", { duration: 4, x: 800, y:200, scale: 2, backgroundColor: "blue" });

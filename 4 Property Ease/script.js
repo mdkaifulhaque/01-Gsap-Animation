c20c6cd025f0a349506972a6209cfcecd5b72594
Kaif <kaif49490@gmail.com>
2025-09-13 21:34:31 +0530

    Property Ease

---
+++ 4 Property Ease/script.js
@@ -0,0 +1,6 @@
+gsap.to(".one", { duration: 2, x: 800, ease: "linear" });
+gsap.to(".two", { duration: 2, x: 800, ease: "bounce.in" });
+gsap.to(".three", { duration: 2, x: 800, ease: "elastic.in" });
+gsap.to(".four", { duration: 2, x: 800, ease: "back.inOut" });
+gsap.to(".five", { duration: 2, x: 800, ease: "expo.in" });
+gsap.to(".six", { duration: 2, x: 800, ease: "sine.inOut" });
